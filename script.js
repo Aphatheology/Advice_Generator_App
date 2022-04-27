1126cf94554997e59e16eaa7ad1b22099b0aed92
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-27 15:18:44 +0100

    Generate automatically on selection

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const adviceBtn = document.querySelector('.advice__dice');
 const downloadAdvice = document.getElementById('advice');
 const downloadBtn = document.querySelector('.download');
 const form = document.querySelector("form");
+const randomQuote = document.querySelector('#quotegen');
+const randomAdvice = document.querySelector('#advicegen')
 
 
-async function generateAdvice() {
-    
+async function generateAdviceOrQuote() {
     adviceBtn.disabled = true;
 
     if(form.elements["gentype"].value === 'advice') {
@@ -32,8 +33,34 @@ async function generateAdvice() {
     }
 }
 
-window.onload = generateAdvice();
-adviceBtn.addEventListener('click' , generateAdvice);
+// Generate an advice once the Advice radio button is selecter
+randomAdvice.addEventListener('change' , async () => {
+    adviceBtn.disabled = true;
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    adviceId.textContent = `Anonymous Advice #${data.slip.id}`;
+    adviceText.textContent = `"${data.slip.advice}"`;
+    setInterval(() => {
+        adviceBtn.disabled = false;
+    }, 2000)
+})
+
+// Generate a quote once the Quote radio button is selecter
+randomQuote.addEventListener('change' , async () => {
+    adviceBtn.disabled = true;
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    adviceId.textContent = `${data.author} said;`;
+    adviceText.textContent = `"${data.content}"`;
+    setInterval(() => {
+        adviceBtn.disabled = false;
+    }, 2000)
+})
+
+window.onload = generateAdviceOrQuote();
+adviceBtn.addEventListener('click' , generateAdviceOrQuote);
+// randomAdvice.addEventListener('click', generateAdviceOrQuote);
+// randomQuote.addEventListener('click', generateAdviceOrQuote);
 
 downloadBtn.addEventListener('click', () => {
     htmlToImage.toPng(downloadAdvice)
